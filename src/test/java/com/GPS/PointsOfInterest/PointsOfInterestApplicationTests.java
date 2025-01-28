066cc931f21d494556605ab510c880b6c96bb122
GabrielSDC <gabrielsantosdacosta2@gmail.com>
2025-01-28 11:24:51 -0300

    initial commit

---
+++ src/test/java/com/GPS/PointsOfInterest/PointsOfInterestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GPS.PointsOfInterest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointsOfInterestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
