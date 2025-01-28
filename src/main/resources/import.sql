3b8b5b3fbc0a9c82bb280a2509498af45517da06
GabrielSDC <gabrielsantosdacosta2@gmail.com>
2025-01-28 17:49:11 -0300

    list all POIs and seed database for tests

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_points_of_interest (name, x, y) VALUES ('lanchonete', 27, 12)
+INSERT INTO tb_points_of_interest (name, x, y) VALUES ('posto', 31, 18)
+INSERT INTO tb_points_of_interest (name, x, y) VALUES ('joalheria', 15, 12)
